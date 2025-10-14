742970d07621f9cd86d00a417ddd13a8d8983400
MrVirul <virul.mmeemana@gmail.com>
2025-10-14 11:03:30 +0530

    Initialize Spring Boot project with Maven configuration, base application structure, and test setup.

---
+++ src/main/java/com/virul/medisure/MedisureApplication.java
@@ -0,0 +1,13 @@
+package com.virul.medisure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedisureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedisureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/virul/medisure/MedisureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virul.medisure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedisureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
